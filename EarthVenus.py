a1869e4716a4144205885f18c44e99b90b4a9967
kaeun <attackmood@gmail.com>
2021-07-04 23:56:31 +0900

    revolve 메쏘드 수정

--- EarthVenus.py
+++ EarthVenus.py
@@ -5,28 +5,30 @@ planets = {
     'venus': {'diameter': 0.949, 'orbit': 150, 'speed': 6.1518496616072005, 'color': 'yellow'},
     'earth': {'diameter': 1.0, 'orbit': 208.33333333333334, 'speed': 10, 'color': 'blue'},
 }
+
 line = Turtle();
 line.ht();
 line.speed(0)
 line.pencolor("#bbb")
+
 def setup_planets(planets):
     for planet in planets:
-        dictionary = planets[planet]
+        dictionary = planets[planet] # planets['venus']
+
         turtle = Turtle(shape='circle')
 
-        turtle.speed("fastest")  # speed controlled elsewhere, disable here
+        turtle.speed("fastest")
         turtle.shapesize(dictionary['diameter'])
         turtle.color(dictionary['color'])
-        turtle.pu()
+        turtle.penup()
         turtle.sety(-dictionary['orbit'])
-        turtle.pd()
+        turtle.pendown()
     
 
         dictionary['turtle'] = turtle
 
     screen.ontimer(revolve, 0)
 
-    screen.mainloop()
 
 def revolve():
     earthD = planets['earth']
@@ -34,9 +36,9 @@ def revolve():
     earthD['turtle'].circle(earthD['orbit'], earthD['speed'])
     venusD['turtle'].circle(venusD['orbit'], venusD['speed'])
     
-    line.pu();
+    line.penup();
     line.setpos(earthD['turtle'].pos())
-    line.pd();
+    line.pendown();
     line.setpos(venusD['turtle'].pos())        
 
     screen.ontimer(revolve, 0)
